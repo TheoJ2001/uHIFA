1d6fb41a71173e827f9f0899c1a8b3cf4dd1f578
TheoJ2001 <32549413+TheoJ2001@users.noreply.github.com>
2020-03-05 13:24:21 +0100

    Add files via upload

---
+++ uHIFA.cpp
@@ -0,0 +1,334 @@
+#include "uHIFA.h"
+
+PISTON::PISTON(){}
+
+void PISTON::initiate(){
+    pinMode(piston_pressure, OUTPUT);		
+    pinMode(ext_sens, INPUT);
+	pinMode(rtd_sens, INPUT);
+}
+
+void PISTON::config(uint8_t type, uint8_t rtd_pin, uint8_t ext_pin, uint8_t piston_pin){
+    pistonType = type; 
+    rtd_sens = rtd_pin;	
+    ext_sens = ext_pin;				
+    piston_pressure = piston_pin;
+    pinMode(rtd_sens, INPUT);
+    pinMode(ext_sens, INPUT);
+    pinMode(piston_pressure, OUTPUT);	
+}
+
+void PISTON::read(){
+	extended = digitalRead(ext_sens);
+	retracted = digitalRead(rtd_sens);
+    if(not PUSH){
+        holding = digitalRead(hold_sens);
+    }
+}
+
+void PISTON::extend(){
+    digitalWrite(piston_pressure, 1);
+    read();
+}
+
+void PISTON::retract(){
+    digitalWrite(piston_pressure, 0); 
+    read();
+}
+
+void PISTON::addGrab(uint8_t hold_pin, uint8_t grab_pin){
+    if(pistonType != PUSH){
+        hold_sens = hold_pin;
+        grab_pressure = grab_pin; 
+		pinMode(hold_sens, INPUT);
+        pinMode(grab_pressure, OUTPUT);
+	}
+}
+
+void PISTON::grab(){
+    if(pistonType == VACUUM){
+        digitalWrite(grab_pressure, 1);
+    }else{
+        if(holding){
+            digitalWrite(grab_pressure, 1);
+        }
+    } 
+    read();
+}
+
+void PISTON::drop(){
+    if(pistonType != PUSH){
+        digitalWrite(grab_pressure, 0); 
+    }
+}
+
+int16_t PISTON::status(uint8_t mode){
+    read();
+    if(mode == RETRACTED){
+        return retracted;
+    }else if(mode == EXTENDED){
+        return extended;
+    }else if(mode == SAFE){
+        if(retracted and not extended){
+            return true;
+        }else{
+            return false;
+        }
+    }else if(mode == HOLDING){
+        return holding;
+    }
+} 
+
+
+SHUTTLE::SHUTTLE(uint8_t upper_pin, uint8_t lower_pin){
+	upper_pressure = upper_pin;	
+	lower_pressure = lower_pin;
+    
+    for(uint8_t i = 0; i<max_stops; i++){ //sets all pins for the stops to undefined
+    	stops[i] = NOT_DEF;
+    }
+}
+
+void SHUTTLE::initiate(){
+    pinMode(upper_pressure, OUTPUT);
+	pinMode(lower_pressure, OUTPUT);
+}
+
+void SHUTTLE::config(uint8_t type, uint8_t rtd_pin, uint8_t ext_pin, uint8_t arm_pin, uint8_t hold_pin, uint8_t grab_pin){
+    shuttle_arm.config(type, rtd_pin, ext_pin, arm_pin);
+	shuttle_arm.addGrab(hold_pin, grab_pin);
+}
+
+void SHUTTLE::addStop(uint8_t s_index, int8_t s_pin){
+	stops[s_index] = s_pin;
+	stops_amt += (stops[s_index] != -1) ? 1:0;
+    pinMode(stops[s_index], INPUT);
+}
+
+void SHUTTLE::read(){
+	for(uint8_t i = 0; i<stops_amt; i++){
+		stop_status[i] = digitalRead(stops[i]);
+		if(stop_status[i] != 0){
+            last_stop = i;
+        }
+	}
+    current_stop = -1;
+    for(uint8_t i = 0; i<stops_amt; i++){
+		stop_status[i] = digitalRead(stops[i]);
+		if(stop_status[i] != 0){
+            current_stop = last_stop;
+        }
+	}
+}
+
+void SHUTTLE::forward(){
+    if(shuttle_arm.status(SAFE)){
+        digitalWrite(upper_pressure, 0);
+        digitalWrite(lower_pressure, 1);
+        moving = true;
+    }else{
+        digitalWrite(upper_pressure, 1);
+        digitalWrite(lower_pressure, 1); 
+    }
+}
+
+void SHUTTLE::backward(){
+	if(shuttle_arm.status(SAFE)){
+        digitalWrite(upper_pressure, 1);
+        digitalWrite(lower_pressure, 0);
+        moving = true;
+    }else{
+        digitalWrite(upper_pressure, 1);
+        digitalWrite(lower_pressure, 1); 
+        moving = false;
+    }
+}
+
+void SHUTTLE::stop(){
+    digitalWrite(upper_pressure, 1);
+    digitalWrite(lower_pressure, 1);
+    moving = false;
+}
+
+void SHUTTLE::maintain(){
+    read();
+}
+
+void SHUTTLE::move(uint8_t pos){
+    if(pos>last_stop){
+        forward();
+    }else if(pos<last_stop){
+        backward();
+    }else if(pos == current_stop){
+        stop();
+    }
+}
+
+
+void SHUTTLE::beginDeliv(uint8_t mode){
+    if(not delivering){
+        if(current_stop != -1){
+            if(mode == EXTENDED){
+               shuttle_arm.extend();
+                if(not shuttle_arm.status(SAFE)){
+                    shuttle_arm.grab();
+                    if(shuttle_arm.status(HOLDING) and shuttle_arm.status(EXTENDED)){
+                        delay(1000);
+                        shuttle_arm.retract();
+                        delivering = true;
+                    } 
+                }else if(mode == RETRACTED){
+                    shuttle_arm.retract();
+                    if(shuttle_arm.status(SAFE)){
+                        shuttle_arm.grab();
+                    }
+                    if(shuttle_arm.status(HOLDING)){
+                        delivering = true;
+                    }  
+                }     
+            }
+        }
+    }
+}
+
+void SHUTTLE::endDeliv(uint8_t mode){
+    if(delivering){
+        if(mode == EXTENDED){
+            shuttle_arm.extend();
+            if(shuttle_arm.status(EXTENDED)){
+                shuttle_arm.drop();
+                if(not shuttle_arm.status(HOLDING)){
+                    shuttle_arm.retract();
+                    delivering = false;
+                } 
+            }  
+        }else if(mode == RETRACTED){
+                shuttle_arm.retract();
+                shuttle_arm.drop();
+                delivering = false;
+        }
+    }
+}
+
+int16_t SHUTTLE::status(uint8_t mode){
+    read();
+    if(mode == POSITION){
+        return last_stop;
+    }else if(mode == MOVING){
+        if(current_stop == -1){
+            return true;
+        }else{
+            return false;
+        }
+    }else if(mode == DELIVERING){
+        return delivering;
+    }else if(mode == SAFE){
+        return shuttle_arm.status(SAFE);
+    }
+}
+
+
+CONVEYOR::CONVEYOR(uint8_t pwr, uint8_t plr, uint8_t str, uint8_t end, uint8_t tachom){
+    power_relay_pin = pwr;
+    polar_relay_pin = plr;
+    start_sens_pin = str;
+    end_sens_pin = end;
+    tachometer_pin = tachom;
+}
+
+void CONVEYOR::initiate(){
+    pinMode(power_relay_pin, OUTPUT);
+    pinMode(polar_relay_pin, OUTPUT);
+    pinMode(start_sens_pin, INPUT);
+    pinMode(end_sens_pin, INPUT);
+    pinMode(tachometer_pin, INPUT_PULLUP);
+}
+
+void CONVEYOR::setMax(uint16_t maxim){
+    tachometer_max = maxim;
+}
+
+void CONVEYOR::start(){
+    digitalWrite(power_relay_pin, 1);
+    stopped = false;
+}
+
+void CONVEYOR::stop(){
+    digitalWrite(power_relay_pin, 0);
+    stopped = true;
+}
+
+void CONVEYOR::forward(){
+    digitalWrite(polar_relay_pin, 0);
+}
+
+void CONVEYOR::backward(){
+    digitalWrite(polar_relay_pin, 1);
+}
+
+void CONVEYOR::reset(){
+    reseting = true;
+    start();
+    backward();
+    if(at_start){
+        stop();
+        tachometer_val = 0;
+        reseting = false;
+        if(req_reset){
+            req_reset = false;
+        }
+    }
+}
+
+void CONVEYOR::maintain(){
+    at_start = digitalRead(start_sens_pin);
+    at_end = digitalRead(end_sens_pin);
+    if(at_end){
+        req_reset = true;
+    }
+    
+    if(not req_reset){
+        if(not reseting and not stopped){
+            if(not tachometer_read){
+                if(digitalRead(tachometer_pin)){
+                    tachometer_val +=1;
+                    tachometer_read = true;
+                }
+            }else{
+                if(not digitalRead(tachometer_pin)){
+                    tachometer_read = false;
+                } 
+            }
+        }
+    }else{
+        reset();
+    }
+}
+
+void CONVEYOR::move(uint16_t pos){
+    start();
+    tachometer_val_mapped = map(tachometer_val, 0, tachometer_max, 0, 100);
+    if(pos>tachometer_val_mapped){
+        forward();
+    }else if(pos<tachometer_val_mapped){
+        req_reset = true;
+    }else{
+        stop();
+    }
+}
+
+int16_t CONVEYOR::status(uint8_t mode){
+    if(mode == RESET_REQ){
+        return req_reset;
+    }else if(mode == RESETING){
+        return reseting;
+    }else if(mode == STOPPED){
+        return stopped;
+    }else if(mode == START){
+        return at_start;
+    }else if(mode == END){
+        return at_end;
+    }else if(mode == POSITION){
+        return tachometer_val_mapped;
+    }
+}
---
+++ uHIFA.h
@@ -0,0 +1,257 @@
+#ifndef uHIFA_h
+#define uHIFA_h
+
+#include <stdint.h> 
+#include <Arduino.h>
+
+#define NOT_DEF -1
+
+#define PUSH 0
+#define CLAW 1
+#define VACUUM 2
+
+#define RETRACTED 0
+#define EXTENDED 1
+#define SAFE 2
+#define HOLDING 3
+#define POSITION 4
+#define MOVING 5
+#define DELIVERING 6
+#define RESET_REQ 7
+#define RESETING 8
+#define STOPPED 9
+#define START 10
+#define END 11
+
+class PISTON{
+  public:
+    PISTON();
+    void initiate();
+    void config(uint8_t type, uint8_t rtd_pin, uint8_t ext_pin, uint8_t piston_pin);
+    void addGrab(uint8_t hold_pin, uint8_t grab_pin);
+    void extend();
+    void retract();
+    void grab();
+    void drop();
+    int16_t status(uint8_t mode);
+  private:
+    uint8_t pistonType;              // 0 for push    
+                                  // 1 for claw
+    				              // 2 for vacuum
+                                    
+    uint8_t ext_sens;			//the sensor that checks if the arm is extended
+    uint8_t rtd_sens;			//the sensor that checks if the arm is retracted
+    uint8_t hold_sens;			//the sensor that checks if the arm is holding an item
+    bool extended;
+    bool retracted;
+    bool holding;
+    uint8_t grab_pressure; 		//the actuator controling the grabing pressure
+    uint8_t piston_pressure;		//the actuator controling the extension and retraction of the arm 
+    
+    void read();
+};
+
+class SHUTTLE{
+  public:
+	  SHUTTLE(uint8_t upper_pin, uint8_t lower_pin); 
+    void initiate();
+	  void config(uint8_t type, uint8_t rtd_pin, uint8_t ext_pin, uint8_t arm_pin, uint8_t hold_pin, uint8_t grab_pin); 
+																				 //arm = pin of actuator extending and retracting arm
+																				 //upper = pin for actuator controling the upper preassure of the shuttle
+																				 //lower = pin for actuator controling the lower preassure of the shuttle
+    void addStop(uint8_t s_index, int8_t s_pin);		 	 //add stops
+    void maintain();
+    void move(uint8_t pos);
+    void beginDeliv(uint8_t mode);
+    void endDeliv(uint8_t mode);
+    
+    int16_t status(uint8_t mode);
+   private:
+    PISTON shuttle_arm;
+    
+    uint8_t max_stops = 8;
+    int8_t stops[8];		        //the pins of the stops, -1 undefined
+    bool stop_status[8];	        //if index is true then it's stopped at index
+    uint8_t stops_amt;				//how many times will the shuttle stop
+    uint8_t upper_pressure;			//the actuator controling the upper pressure of the shuttle 
+    uint8_t lower_pressure;			//the actuator controling the lower pressure of the shuttle  
+    
+    int8_t last_stop = -1;
+    int8_t current_stop;
+    bool delivering = false; 
+    bool moving = false;
+        
+    void read();
+    void stop();
+    void forward();
+    void backward();
+};
+
+class CONVEYOR{
+  public:
+    CONVEYOR(uint8_t pwr, uint8_t plr, uint8_t str, uint8_t end, uint8_t tachom);
+    void initiate();
+    void setMax(uint16_t maxim);
+    void maintain();
+    void reset();
+    void move(uint16_t pos);
+    int16_t status(uint8_t mode);
+    
+  private:
+    uint8_t power_relay_pin;
+    uint8_t polar_relay_pin;
+    uint8_t tachometer_pin;
+    uint8_t start_sens_pin;
+    uint8_t end_sens_pin;
+    
+    bool at_start;
+    bool at_end;
+    
+    bool req_reset = true;
+    bool reseting;
+    
+    bool stopped;
+    
+    uint16_t tachometer_max;
+    uint16_t tachometer_val;
+    uint16_t tachometer_val_mapped;
+    
+    bool tachometer_read;
+    
+    void start();
+    void stop();
+    void forward();
+    void backward();
+};
+#endif
+
+#ifdef CONTROLLINO_MINI
+  #define _D0 4
+  #define _D1 5
+  #define _D2 6
+  #define _D3 7
+  #define _D4 8
+  #define _D5 9
+  #define _D6 18
+  #define _D7 19
+  
+  #define _A0 14
+  #define _A1 15
+  #define _A2 16
+  #define _A3 17
+  #define _A4 20
+  #define _A5 21
+  #define _IN0 2
+  #define _IN1 3
+#endif
+
+#ifdef CONTROLLINO_MAXI
+  #define _D0 2
+  #define _D1 3
+  #define _D2 4
+  #define _D3 5
+  #define _D4 6
+  #define _D5 7
+  #define _D6 8
+  #define _D7 9
+  #define _D8 10
+  #define _D9 11
+  #define _D10 12
+  #define _D11 13
+  
+  #define _A0 54
+  #define _A1 55
+  #define _A2 56
+  #define _A3 57
+  #define _A4 58
+  #define _A5 59
+  #define _A6 60
+  #define _A7 61
+  #define _A8 62
+  #define _A9 63
+  #define _IN0 18
+  #define _IN1 19
+  
+  #define _MINUS 14
+  #define _PLUS 15
+  
+  #define _R0 22
+  #define _R1 23
+  #define _R2 24
+  #define _R3 25
+  #define _R4 26
+  #define _R5 27
+  #define _R6 28
+  #define _R7 29
+  #define _R8 30
+  #define _R9 31
+#endif
+
+#ifdef CONTROLLINO_MEGA
+  #define _D0 2
+  #define _D1 3
+  #define _D2 4
+  #define _D3 5
+  #define _D4 6
+  #define _D5 7
+  #define _D6 8
+  #define _D7 9
+  #define _D8 10
+  #define _D9 11
+  #define _D10 12
+  #define _D11 13
+  #define _D12 42
+  #define _D13 43
+  #define _D14 44
+  #define _D15 45
+  #define _D16 46
+  #define _D17 47
+  #define _D18 48
+  #define _D19 49
+  #define _D20 77
+  #define _D21 78
+  #define _D22 79
+  #define _D23 80
+  
+  #define _A0 54
+  #define _A1 55
+  #define _A2 56
+  #define _A3 57
+  #define _A4 58
+  #define _A5 59
+  #define _A6 60
+  #define _A7 61
+  #define _A8 62
+  #define _A9 63
+  #define _A10 64
+  #define _A11 65
+  #define _A12 66
+  #define _A13 67
+  #define _A14 68
+  #define _A15 69
+  #define _I16 38
+  #define _I17 39
+  #define _I18 40
+  #define _IN0 18
+  #define _IN1 19
+  
+  #define _MINUS 14
+  #define _PLUS 15
+  
+  #define _R0 22
+  #define _R1 23
+  #define _R2 24
+  #define _R3 25
+  #define _R4 26
+  #define _R5 27
+  #define _R6 28
+  #define _R7 29
+  #define _R8 30
+  #define _R9 31
+  #define _R10 32
+  #define _R11 33
+  #define _R12 34
+  #define _R13 35
+  #define _R14 36
+  #define _R15 37
+#endif
